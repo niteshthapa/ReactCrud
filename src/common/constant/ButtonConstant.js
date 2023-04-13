0c1d86260f351551621b980c25a99c210c7c31d3
niteshthapa <nitesh_thapa@ymail.com>
2023-04-13 22:14:31 +0530

    Create ButtonConstant.js

---
+++ src/common/constant/ButtonConstant.js
@@ -0,0 +1,6 @@
+export const BUTTON_ADD= "Add";
+export const BUTTON_CANCEL = "Cancel";
+export const BUTTON_EDIT = "Edit";
+export const BUTTON_DELETE = "Delete";
+export const BUTTON_UPDATE = "Update";
+export const BUTTON_OK = "Ok";
