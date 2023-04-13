455cac5871adb7dd3f68e7c94343dc13138ed27d
niteshthapa <nitesh_thapa@ymail.com>
2023-04-13 22:25:50 +0530

    Create Types.js

---
+++ src/redux/Types.js
@@ -0,0 +1,7 @@
+
+export const ADD_USER = "ADD_USER";
+export const DELETE_USER = "DELETE_TODO";
+export const UPDATE_USER = "UPDATE_TODO";
+export const FETCH_USER = "FETCH_USER";
+export const ERROR_SAVING = "ERROR_SAVING";
+export const ALERT_MESSAGE = "ALERT_MESSAGE";
