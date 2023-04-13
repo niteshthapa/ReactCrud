f19c3fc9b2c5807ba60edfa28c80dbece969a7ea
niteshthapa <nitesh_thapa@ymail.com>
2023-04-13 22:15:37 +0530

    Create ServiceConstant.js

---
+++ src/common/constant/ServiceConstant.js
@@ -0,0 +1,6 @@
+const APPLICATION_API_BASE_PATH = 'https://retoolapi.dev/';
+const APPLICATION_API_CODE = 'LrLTcY/';
+export const ServiceConstant = {
+   API_PATH: `${APPLICATION_API_BASE_PATH}`,
+   API_UNIQUE_CODE: `${APPLICATION_API_CODE}`,
+}
